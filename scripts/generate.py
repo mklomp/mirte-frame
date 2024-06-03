2d141d97334427d23379de286af9a2bb4af97663
mklomp <m.klomp@tudelft.nl>
2024-06-03 21:16:00 +0200

    Update generate.py

--- scripts/generate.py
+++ scripts/generate.py
@@ -105,9 +105,7 @@ import os
 import shutil
 from pathlib import Path
 
-dir_path = "./"
-if '__file__' in globals():
-   dir_path = Path(__File__).resolve().parent
+dir_path = Path(os.getcwd())
 freecad_directory = (dir_path / rel_freecad_directory).resolve()
 
 # clean build directory
