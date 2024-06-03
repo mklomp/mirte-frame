ddc9c8b726cff2ff48c504709545caa84740579d
mklomp <m.klomp@tudelft.nl>
2024-06-03 20:45:10 +0200

    Filed file name error

--- scripts/generate.py
+++ scripts/generate.py
@@ -104,7 +104,7 @@ def renderFile(freecadFile):
 import os
 import shutil
 from pathlib import Path
-dir_path = Path(__file__).resolve().parent
+dir_path = Path(__File__).resolve().parent
 freecad_directory = (dir_path / rel_freecad_directory).resolve()
 
 # clean build directory
